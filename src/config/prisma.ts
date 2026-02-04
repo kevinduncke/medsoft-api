3ef28f777a46d1fb9028d42e30bb1c0d975be4b2
Kevin Duncke <kevinduncke@gmail.com>
2026-02-04 16:52:21 -0300

    Updated .gitignore and prisma.ts

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,11 +1,3 @@
-import { PrismaClient, PrismaClientOptions } from '../generated/prisma/client';
+import { PrismaClient } from '@prisma/client'
 
-const prismaClientOptions: PrismaClientOptions = {
-    log: ['query', 'info', 'warn', 'error'],
-};
-
-if (process.env.PRISMA_ACCELERATE_URL) {
-    prismaClientOptions.accelerateUrl = process.env.PRISMA_ACCELERATE_URL;
-}
-
-export const prisma = new PrismaClient(prismaClientOptions);
+export const prisma = new PrismaClient()
