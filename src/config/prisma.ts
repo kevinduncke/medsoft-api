bc29555f60381c2b89d0d7bb3464e64204fb8f93
Kevin Duncke <kevinduncke@gmail.com>
2026-02-04 17:07:34 -0300

    Update #7

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,3 +1,8 @@
-import { PrismaClient } from '@prisma/client'
+import dotenv from 'dotenv';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from './generated/client';
 
-export const prisma = new PrismaClient()
+dotenv.config();
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+export const prisma = new PrismaClient({ adapter});
